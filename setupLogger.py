b7a14d1e9ae577351e8b815664034df9f5b641d8
Lana Abadie <lana.abadie@iter.org>
2021-01-25 10:34:47 +0100

    review the logging strategy to add client ip and username + remove the pid folder to ease clean up

--- setupLogger.py
+++ setupLogger.py
@@ -1,9 +1,26 @@
 import logging
 import os
 import errno
+import platform
+import getpass
 from logging.handlers import TimedRotatingFileHandler
 from pathlib import Path
 
+
+class HostnameFilter(logging.Filter):
+    hostname = platform.node()
+
+    def filter(self, record):
+        record.hostname = HostnameFilter.hostname
+        return True
+
+
+class UserFilter(logging.Filter):
+    username = getpass.getuser()
+
+    def filter(self, record):
+        record.username = UserFilter.username
+        return True
 
 def get_file_handler():
     userLoc = os.environ.get('PROTO_LOG_PATH')
@@ -17,9 +34,9 @@ def get_file_handler():
         dfile = "protoplotQt5.log"
     else:
         dfile = userFile
-    FORMATTER = logging.Formatter("%(asctime)s.%(msecs)03d-%(levelname)s-%(name)s-%(funcName)s-%(message)s", datefmt='%Y-%m-%dT%H:%M:%S')
-    pid = os.getpid()
-    cusFolder = dpath + "/p" + str(pid) + "/logs"
+    FORMATTER = logging.Formatter("[%(asctime)s.%(msecs)03d][%(hostname)s:%(username)s][%(processName)s:%(process)d][%(name)s-%(funcName)s][%(levelname)s]%(message)s", datefmt='%Y-%m-%dT%H:%M:%S')
+
+    cusFolder = dpath + "/logs"
     #logging.config.fileConfig('logging.conf')
     #logging.basicConfig( format='%(asctime)s-%(levelname)s-%(process)d-%(funcName)s-%(message)s', datefmt='%Y-%m-%dT%H:%M:%S')
     #filename="~/.local/1Dtool/logs/protoplotQt5.log",
@@ -31,6 +48,8 @@ def get_file_handler():
             raise
     filename = cusFolder + "/" + dfile
     file_handler = TimedRotatingFileHandler(filename, when='D', interval=1, backupCount=10,encoding='utf-8')
+    file_handler.addFilter(HostnameFilter())
+    file_handler.addFilter(UserFilter())
     file_handler.setFormatter(FORMATTER)
     return file_handler
 
