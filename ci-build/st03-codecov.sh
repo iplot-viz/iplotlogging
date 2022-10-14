0ed33c03a563b6db7300e561d7dbcf97a9e9b2d4
Sawantdesai Prasad parshuram EXT <Prasadparshuram.Sawantdesai@iter.org>
2022-10-14 13:37:13 +0200

    IDV-396 updated package name and tests folder for coverage

--- ci-build/st03-codecov.sh
+++ ci-build/st03-codecov.sh
@@ -10,7 +10,7 @@ tar -xvzf ${PREFIX_DIR}.tar.gz ./${PREFIX_DIR}
 
 export PYTHONPATH=${PYTHONPATH}:$(get_abs_filename "./${PREFIX_DIR}")
 # run tests
-coverage run --source=iplotLogger -m pytest iplotLogging
+coverage run --source=iplotLogging -m pytest tests
 
 # report
 coverage report -i
