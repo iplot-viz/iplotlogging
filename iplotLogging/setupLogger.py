2a0134a3a8c381a822406e2acf199192cff42908
martinp6 <pmartin@minsait.com>
2023-07-20 08:08:52 +0000

    IDV-364 Apply PEP-8 to iplotLogging

--- iplotLogging/setupLogger.py
+++ iplotLogging/setupLogger.py
@@ -7,6 +7,7 @@ import sys
 from logging.handlers import TimedRotatingFileHandler
 from pathlib import Path
 
+
 class HostnameFilter(logging.Filter):
     hostname = platform.node()
 
@@ -23,8 +24,6 @@ class UserFilter(logging.Filter):
         return True
 
 
-
-
 def formatLevel(lvl):
     if lvl == "INFO":
         return logging.INFO
@@ -39,46 +38,43 @@ def formatLevel(lvl):
 
     return logging.INFO
 
+
 def get_file_handler():
-    userLoc = os.environ.get('PROTO_LOG_PATH')
-    if userLoc is None:
+    user_loc = os.environ.get('PROTO_LOG_PATH')
+    if user_loc is None:
         home = str(Path.home())
         dpath = home + "/.local/1Dtool/"
     else:
-        dpath = userLoc
-    userFile = os.environ.get('PROTO_LOG_FILENAME')
-    if userFile is None:
+        dpath = user_loc
+    user_file = os.environ.get('PROTO_LOG_FILENAME')
+    if user_file is None:
         dfile = f"mint_{platform.node()}_{os.getpid()}.log"
     else:
-        dfile = userFile
-    FORMATTER = logging.Formatter(
-        "[%(asctime)s.%(msecs)03d][%(hostname)s:%(username)s][%(processName)s:%(process)d][%(name)s-%(funcName)s][%(levelname)s]%(message)s",
-        datefmt='%Y-%m-%dT%H:%M:%S')
+        dfile = user_file
+    formatter = logging.Formatter("[%(asctime)s.%(msecs)03d][%(hostname)s:%(username)s][%(processName)s:%(process)d]"
+                                  "[%(name)s-%(funcName)s][%(levelname)s]%(message)s", datefmt='%Y-%m-%dT%H:%M:%S')
 
-    cusFolder = dpath + "/logs"
+    cus_folder = dpath + "/logs"
     # logging.config.fileConfig('logging.conf')
-    # logging.basicConfig( format='%(asctime)s-%(levelname)s-%(process)d-%(funcName)s-%(message)s', datefmt='%Y-%m-%dT%H:%M:%S')
+    # logging.basicConfig( format='%(asctime)s-%(levelname)s-%(process)d-%(funcName)s-%(message)s',
+    #                      datefmt='%Y-%m-%dT%H:%M:%S')
     # filename="~/.local/1Dtool/logs/protoplotQt5.log",
-    logger = logging.getLogger('root')
+    # logger = logging.getLogger('root')
     try:
-        os.makedirs(cusFolder)
+        os.makedirs(cus_folder)
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
-    filename = cusFolder + "/" + dfile
+    filename = cus_folder + "/" + dfile
     file_handler = TimedRotatingFileHandler(filename, when='D', interval=1, backupCount=10, encoding='utf-8')
     file_handler.addFilter(HostnameFilter())
     file_handler.addFilter(UserFilter())
-    file_handler.setFormatter(FORMATTER)
+    file_handler.setFormatter(formatter)
     return file_handler
 
-class fileHandlerIplot(object):
 
+class FileHandlerIplot(object):
     fhandler = get_file_handler()
-
-
-
-
 
 
 def get_logger(logger_name, level=None):
@@ -86,13 +82,13 @@ def get_logger(logger_name, level=None):
     if level:
         llevel = formatLevel(level)
     else:
-        llevelX = os.environ.get('PROTO_LOG_LEVEL')
-        if llevelX is None:
+        llevel_x = os.environ.get('PROTO_LOG_LEVEL')
+        if llevel_x is None:
             llevel = logging.INFO
         else:
-            llevel = formatLevel(llevelX)
+            llevel = formatLevel(llevel_x)
 
     logger.setLevel(llevel)
-    logger.addHandler(fileHandlerIplot.fhandler)
+    logger.addHandler(FileHandlerIplot.fhandler)
     logger.addHandler(logging.StreamHandler(sys.stdout))
     return logger
