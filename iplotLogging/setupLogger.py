a21a4305caf02cc2de1083aeeae46350b6dc21f2
pmartin <pmartin@minsait.com>
2024-01-25 10:13:25 +0100

    IDV-387 Now when it starts remove the older log files

--- iplotLogging/setupLogger.py
+++ iplotLogging/setupLogger.py
@@ -4,6 +4,7 @@ import errno
 import platform
 import getpass
 import sys
+import datetime
 from logging.handlers import TimedRotatingFileHandler
 from pathlib import Path
 
@@ -24,7 +25,7 @@ class UserFilter(logging.Filter):
         return True
 
 
-def formatLevel(lvl):
+def format_level(lvl):
     if lvl == "INFO":
         return logging.INFO
     if lvl == "DEBUG":
@@ -40,17 +41,10 @@ def formatLevel(lvl):
 
 
 def get_file_handler():
-    user_loc = os.environ.get('PROTO_LOG_PATH')
-    if user_loc is None:
-        home = str(Path.home())
-        dpath = home + "/.local/1Dtool/"
-    else:
-        dpath = user_loc
-    user_file = os.environ.get('PROTO_LOG_FILENAME')
-    if user_file is None:
-        dfile = f"mint_{platform.node()}_{os.getpid()}.log"
-    else:
-        dfile = user_file
+    dpath = os.environ.get('PROTO_LOG_PATH') or f"{Path.home()}/.local/1Dtool"
+
+    dfile = os.environ.get('PROTO_LOG_FILENAME') or f"mint_{platform.node()}_{os.getpid()}.log"
+
     formatter = logging.Formatter("[%(asctime)s.%(msecs)03d][%(hostname)s:%(username)s][%(processName)s:%(process)d]"
                                   "[%(name)s-%(funcName)s][%(levelname)s]%(message)s", datefmt='%Y-%m-%dT%H:%M:%S')
 
@@ -73,22 +67,46 @@ def get_file_handler():
     return file_handler
 
 
-class FileHandlerIplot(object):
-    fhandler = get_file_handler()
-
-
 def get_logger(logger_name, level=None):
     logger = logging.getLogger(logger_name)
     if level:
-        llevel = formatLevel(level)
+        level2 = format_level(level)
     else:
-        llevel_x = os.environ.get('PROTO_LOG_LEVEL')
-        if llevel_x is None:
-            llevel = logging.INFO
+        level2_x = os.environ.get('PROTO_LOG_LEVEL')
+        if level2_x is None:
+            level2 = logging.INFO
         else:
-            llevel = formatLevel(llevel_x)
+            level2 = format_level(level2_x)
 
-    logger.setLevel(llevel)
+    logger.setLevel(level2)
     logger.addHandler(FileHandlerIplot.fhandler)
     logger.addHandler(logging.StreamHandler(sys.stdout))
     return logger
+
+
+def delete_older_logs(logger):
+    path = os.environ.get('PROTO_LOG_PATH') or f"{Path.home()}/.local/1Dtool"
+    days = os.environ.get('LIMIT_DAYS_LOGS_MINT') or 10
+    actual_date = datetime.datetime.now()
+    files = os.listdir(path)
+
+    for file in files:
+        full_path = os.path.join(path, file)
+
+        if os.path.isfile(full_path):
+            file_date = datetime.datetime.fromtimestamp(os.path.getmtime(full_path))
+
+            days_diff = (actual_date - file_date).days
+
+            if days_diff > days:
+                try:
+                    os.remove(full_path)
+                    logger.info(f"Deleted file: {file}")
+                except PermissionError:
+                    logger.warning(f"Permission error deleting file: {file}")
+                except Exception as e:
+                    logger.warning(f"Failed while deleting file: {file} -> {e}")
+
+
+class FileHandlerIplot(object):
+    fhandler = get_file_handler()
